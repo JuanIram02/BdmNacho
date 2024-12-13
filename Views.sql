037b86a1dde7bf47d4e2c0c2b0b153b6708abeb6
JuanIram02 <juanlala02@hotmail.com>
2024-12-12 23:23:55 -0600

    Update Views.sql

--- Views.sql
+++ Views.sql
@@ -27,3 +27,31 @@ ON
 //
 
 DELIMITER ;
+
+DELIMITER //
+
+CREATE OR REPLACE VIEW vwPagos_Inscripciones AS
+SELECT 
+    p.id AS pago_id,
+    p.curso_id,
+    p.estudiante_id,
+    p.monto AS monto_pago,
+    p.forma_pago,
+    p.fecha AS fecha_pago,
+    i.id AS inscripcion_id,
+    i.fecha_inscripcion,
+    i.fecha_ultima,
+    i.fecha_terminacion,
+    i.progreso_curso,
+    i.estado AS estado_inscripcion,
+    i.precio_pagado
+FROM 
+    Pago p
+INNER JOIN 
+    Inscripcion i
+ON 
+    p.curso_id = i.curso_id 
+    AND p.estudiante_id = i.estudiante_id;
+//
+
+DELIMITER ;
